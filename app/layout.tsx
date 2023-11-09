637e11010fa9a75c03409d70bc90d87796754d8b
rogermarco <marcrogers90@gmail.com>
2023-11-09 18:29:47 +0100

    refactor form appearance and rendering

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,11 +9,7 @@ export const metadata: Metadata = {
   description: 'A website to create events',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={`${inter.className} bg-orange-50`}>{children}</body>
--- app/ui/event-form.tsx
+++ app/ui/event-form.tsx
@@ -1,37 +1,66 @@
+'use client'
+
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 function EventForm () {
+const [title, setTitle] = useState('');
+const [mode, setMode] = useState('');
+const [location, setLocation] = useState('');
+
+const modesArray = ['Autocracy', 'Democracy'];
+const locationArray = ['I know where', 'I want help'];
+
   return ( 
     <div className='w-full h-full flex'>
       <form className='m-auto'>
         <h1 className='text-center'>Start a party!</h1>
         <div className='form-control'>
           <h2>Title</h2>
-          <input type='text' name='title' required={true} className='w-full'>
-          </input>
+          <input
+          type='text' 
+          name='title' 
+          required={true} 
+          value={title}
+          className='w-full'
+          onChange={e => setTitle(e.target.value)}
+          />
         </div>
 
         <h2>Pick a date or ask your guests</h2>
         <div className='form-control flex justify-around'>
-          <div>
-            <input type='radio' name='mode'/>
-            <label className='px-3'>Autocracy mode</label>
-          </div>
-          <div>
-            <input type='radio' name='mode'/>
-            <label className='pl-3'>Democracy mode</label>
-          </div>
+          {/* MODE SELECTION */}
+          {modesArray.map((mode, index) => {
+            return (
+              <div key={index}>
+                <input
+                key={index}
+                type='radio'
+                name='mode'
+                onChange={() => setMode(mode)}
+                />
+                <label>{`${mode} mode`}</label>
+              </div>
+            )
+          })}
         </div>
 
         <h2>Pick the location</h2>
-        <div className='form-control flex justify-around'>
-          <div>
-            <input type='radio' name='location' />
-            <label className='px-3'>I know where</label>
-          </div>
-          <div>
-            <input type='radio' name='location' />
-            <label className='pl-3'>I want help</label>
-          </div>
+        <div className='form-control flex'>
+          {/* LOCATION SELECTION */}
+          {locationArray.map((location, index) => {
+            return (
+              <div key={index}>
+                <input
+                key={index}
+                type='radio'
+                name='location'
+                onChange={() => setLocation(location)}
+                />
+                <label>{`${location}`}</label>
+              </div>
+            )
+          })}
         </div>
       </form>
     </div>
