2d31b510aeabc2a528ff8e5ebaed3bc8066f75db
rogermarco <marcrogers90@gmail.com>
2023-11-10 10:16:30 +0100

    add new favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'sayWhen',
+  title: 'Say When',
   description: 'A website to create events',
 }
 
