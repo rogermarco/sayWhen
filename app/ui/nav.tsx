5e2ea5c46bd21c96e19c1c043750dce9c4af8e25
rogermarco <marcrogers90@gmail.com>
2023-11-14 17:05:19 +0100

    add nav styling

--- app/ui/nav.tsx
+++ app/ui/nav.tsx
@@ -1,11 +1,27 @@
+import Image from "next/image";
+import logoBig from '../../public/logo-big.png';
+import Link from "next/link";
+
 function Nav () {
   return ( 
-    <main className="py-10 px-10 flex space-x-4 border-b-2 border-black">
-      <p className="grow">Logo</p>
-      <p>Login</p>
-      <p>Home</p>
+    <>
+    <main className="px-5 py-2 flex items-center space-x-4 fixed top-0 w-full border-b border-black text-logo-orange bg-gradient-to-r from-logo-orange to-40% to-logo-blue">
+      <div className="flex-grow">
+        <Image
+          src={logoBig}
+          height={65}
+          width={190}
+          alt='Logo'
+        />
+      </div>
+
+      <div className="mode-buttons w-28 text-black"><p className="text-center w-full">Login</p></div>
+
+      <Link href='/'>
+        <div className="mode-buttons w-28 text-black"><p className="text-center w-full">Home</p></div>
+      </Link>
     </main>
-    
+    </>
   );
 }
 
